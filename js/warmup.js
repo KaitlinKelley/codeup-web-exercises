08cd14df1dd3b07ec29e4faad2f727c6833e040f
Kaitlin Muse <kaitlinmuse@Kaitlins-MacBook-Air.local>
2020-09-28 09:34:23 -0500

    created warmup files

---
+++ js/warmup.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Warmups");
+console.log("Hello, Foster!");
+console.log("Hello, Nimoy!");
+console.log("Hello, Stanzi!");
+console.log("Hello, Jon Snow!");
+console.log("Hello, Frodo!");
+
+function sayHello(name){
+    console.log("Hello, " + name);
+}
+
+sayHello("Kaitlin");
+//calling the function
+
+
