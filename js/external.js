8f232f1d661b02fb35ef4ad9c9b2ffb1ab564217
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-09-28 10:35:27 -0500

    fixed GitHub stuff + code

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("Hello from external JavaScript.");
+
+var userColor = prompt("What is your favorite color?");
+
+alert("Great! " + userColor + " is my favorite color, too!");
+
+var littleMermaidDays = Number(prompt("How many days did you rent The Little Mermaid for?"));
+var brotherBearDays = Number(prompt("How many days did you rent Brother Bear for?"));
+var herculesDays = Number(prompt("How many days did you rent Hercules for?"));
