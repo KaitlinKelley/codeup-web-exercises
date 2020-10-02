d92e837b3a1c9655a52405aaf0366d3649dc9514
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-10-02 14:35:10 -0500

    doing loops exercises

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -152,24 +152,44 @@
 
 // TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
 
+// for (var i = 0; i <= 5; i++){
+//     console.log(i);
+// }
+
 
 // TODO TOGETHER: Why are loops beneficial?
+//Can do things over and over again without repeating yourself
 
 
 // TODO TOGETHER: Console.log each color from the array randomColors
 //  var randomColors = ["red","orange","yellow","blue","green"]
-
-
+//
+// //iteration: going over each item in a list
+//
+// //randomColors.length = 5, last index of array = 4
+// //useful if we don't know how many elements are in the array
 // for (var i = 0; i < randomColors.length; i++){
-// 	console.log(randomColors[i]);;
+// 	console.log(randomColors[i]);
 // }
 
 
 
 // TODO TOGETHER: Using a for loop, log all even numbers 0 - 100;
 
+// for (var i = 0; i <= 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
 
 // TODO: Using a for loop, log all numbers divisible by 5, from 0 - 100;
+
+// for (var i = 0; i <= 100; i++){
+//     if(i % 5 === 0){
+//         console.log(i);
+//     }
+// }
 
 
 // TODO: Using a for loop, complete the FizzBuzz challenge:
@@ -178,6 +198,22 @@
 //  For numbers divisible by 5, log "Buzz"
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
+
+// for (var i = 0; i <= 100; i++){
+//     if((i % 3 === 0) && (i % 5 === 0)) {    //or if(i % 15 === 0)
+//         console.log("FizzBuzz");
+//     } else if(i % 5 === 0){
+//         console.log("Buzz");
+//     } else if(i % 3 === 0){
+//         console.log("Fizz");
+//     }  else {
+//         console.log(i);
+//     }
+//
+//     }
+
+// can be refactored to a switch statement
+
 
 
 
@@ -196,20 +232,22 @@
 // 	if (i === numberToStopAt) {
 // 		console.log('We have reached the stopping point: break!');
 // 		// use the break keyword to exit from the while loop
-// 		break;
+// 		break; //like a force-quit, nothing will happen after
 // 		// nothing after the break will get processed
 // 		console.log('You will never see this line.');
 // 	}
 // }
 
 
-// We can get an iteration to skip using continue;
-// for (var i = 1; i < 100; i++) {
-//
-// 	if (i % 2 !== 0) {
-// 		// skipping all odd numbers;
-// 		continue;
-// 	}
-//
-// 	console.log('Here is a lovely even number: ' + i);
-// }
+//We can get an iteration to skip using continue;
+for (var i = 1; i < 100; i++) {
+
+	if (i % 2 !== 0) {
+		// skipping all odd numbers;
+        console.log("skipping this odd number");
+		continue; //skip this iteration, go back to the beginning of the loop
+        console.log("should never ever see this");
+	}
+
+	console.log('Here is a lovely even number: ' + i);
+}
