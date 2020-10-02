4a07966ad6ce425d34fa73d0be9c259f6c578063
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-10-02 08:54:18 -0500

    did some edabit

--- onlinePractice.js
+++ onlinePractice.js
@@ -7,3 +7,23 @@ function intToString(num) {
 function stringToInt(num) {
     return +num;
 }
+
+//Given two arguments, return an array which contains these two arguments.
+
+function makePair(num1, num2) {
+    return [num1, num2];
+}
+
+//Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
+//Area of a triangle is 0.5 * b * h
+// Area of a parallelogram is b * h
+// Assume triangle and parallelogram are the only inputs for shape.
+
+function areaShape(base, height, shape) {
+    if(shape === "triangle"){
+        return (base*height)/2;
+    } else if(shape === "parallelogram"){
+        return base*height;
+    }
+}
+
