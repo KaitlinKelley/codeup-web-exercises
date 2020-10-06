517029d614b26ea6b05bf6d5ac01f95b4e678204
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-10-06 12:13:45 -0500

    finished my attempt at first phone number exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -33,3 +33,13 @@
     console.log(planetStringUl);
 
 })();
+
+// BONUS CHALLENGE (when finished with the exercise)...
+// Create a phone number parser that will convert a string of numbers into a human readable format. The output should depend on the number of digits (account for 7, 10, 11 digit numbers):
+// Using multiple functions will be helpful.
+//     input = 5552324343, output = 555-232-4343
+// input = 5553434, output = 555-3434
+// input = 18005552323, output = 1-800-555-2323
+// Extra Challenge: account for invalid characters
+// Extra Challenge: if the input is already formatted, output the unformatted version
+// Extra Challenge: allow the parser to accept letters and convert them to the correct numbers
